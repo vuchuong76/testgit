a0969822bbf4f0196365fe89a9508ccab6c14f39
chuong <vuchuong76@gmail.com>
2024-07-17 23:32:14 +0900

    First commit

---
+++ build/Desktop_Qt_6_7_2_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_1-1TeaserDemo_FILE C:/Users/user/Documents/1-1TeaserDemo/build/Desktop_Qt_6_7_2_MinGW_64_bit-Debug/1-1TeaserDemo.exe)
+set(__QT_DEPLOY_TARGET_1-1TeaserDemo_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_1-1TeaserDemo_RUNTIME_DLLS C:/Qt/6.7.2/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.7.2/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.7.2/mingw_64/bin/Qt6Core.dll)
---
+++ build/Desktop_Qt_6_7_2_MinGW_64_bit-Debug/1-1TeaserDemo_autogen/include/ui_widget.h
@@ -0,0 +1,137 @@
+/********************************************************************************
+** Form generated from reading UI file 'widget.ui'
+**
+** Created by: Qt User Interface Compiler version 6.7.2
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_WIDGET_H
+#define UI_WIDGET_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QHBoxLayout>
+#include <QtWidgets/QLabel>
+#include <QtWidgets/QLineEdit>
+#include <QtWidgets/QPlainTextEdit>
+#include <QtWidgets/QPushButton>
+#include <QtWidgets/QSpacerItem>
+#include <QtWidgets/QVBoxLayout>
+#include <QtWidgets/QWidget>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_Widget
+{
+public:
+    QVBoxLayout *verticalLayout_2;
+    QVBoxLayout *verticalLayout;
+    QHBoxLayout *horizontalLayout;
+    QLabel *label;
+    QLineEdit *firstNameLineEdit;
+    QHBoxLayout *horizontalLayout_2;
+    QLabel *label_3;
+    QLineEdit *lastNameLineEdit;
+    QHBoxLayout *horizontalLayout_3;
+    QLabel *label_2;
+    QPlainTextEdit *messageTextEdit;
+    QHBoxLayout *horizontalLayout_4;
+    QSpacerItem *horizontalSpacer;
+    QPushButton *pushButton;
+
+    void setupUi(QWidget *Widget)
+    {
+        if (Widget->objectName().isEmpty())
+            Widget->setObjectName("Widget");
+        Widget->resize(700, 314);
+        verticalLayout_2 = new QVBoxLayout(Widget);
+        verticalLayout_2->setObjectName("verticalLayout_2");
+        verticalLayout = new QVBoxLayout();
+        verticalLayout->setObjectName("verticalLayout");
+        horizontalLayout = new QHBoxLayout();
+        horizontalLayout->setObjectName("horizontalLayout");
+        label = new QLabel(Widget);
+        label->setObjectName("label");
+
+        horizontalLayout->addWidget(label);
+
+        firstNameLineEdit = new QLineEdit(Widget);
+        firstNameLineEdit->setObjectName("firstNameLineEdit");
+
+        horizontalLayout->addWidget(firstNameLineEdit);
+
+
+        verticalLayout->addLayout(horizontalLayout);
+
+        horizontalLayout_2 = new QHBoxLayout();
+        horizontalLayout_2->setObjectName("horizontalLayout_2");
+        label_3 = new QLabel(Widget);
+        label_3->setObjectName("label_3");
+
+        horizontalLayout_2->addWidget(label_3);
+
+        lastNameLineEdit = new QLineEdit(Widget);
+        lastNameLineEdit->setObjectName("lastNameLineEdit");
+
+        horizontalLayout_2->addWidget(lastNameLineEdit);
+
+
+        verticalLayout->addLayout(horizontalLayout_2);
+
+        horizontalLayout_3 = new QHBoxLayout();
+        horizontalLayout_3->setObjectName("horizontalLayout_3");
+        label_2 = new QLabel(Widget);
+        label_2->setObjectName("label_2");
+
+        horizontalLayout_3->addWidget(label_2);
+
+        messageTextEdit = new QPlainTextEdit(Widget);
+        messageTextEdit->setObjectName("messageTextEdit");
+
+        horizontalLayout_3->addWidget(messageTextEdit);
+
+
+        verticalLayout->addLayout(horizontalLayout_3);
+
+
+        verticalLayout_2->addLayout(verticalLayout);
+
+        horizontalLayout_4 = new QHBoxLayout();
+        horizontalLayout_4->setObjectName("horizontalLayout_4");
+        horizontalSpacer = new QSpacerItem(40, 20, QSizePolicy::Policy::Expanding, QSizePolicy::Policy::Minimum);
+
+        horizontalLayout_4->addItem(horizontalSpacer);
+
+        pushButton = new QPushButton(Widget);
+        pushButton->setObjectName("pushButton");
+
+        horizontalLayout_4->addWidget(pushButton);
+
+
+        verticalLayout_2->addLayout(horizontalLayout_4);
+
+
+        retranslateUi(Widget);
+
+        QMetaObject::connectSlotsByName(Widget);
+    } // setupUi
+
+    void retranslateUi(QWidget *Widget)
+    {
+        Widget->setWindowTitle(QCoreApplication::translate("Widget", "Widget", nullptr));
+        label->setText(QCoreApplication::translate("Widget", "First Name", nullptr));
+        label_3->setText(QCoreApplication::translate("Widget", "Last Name", nullptr));
+        label_2->setText(QCoreApplication::translate("Widget", "Message", nullptr));
+        pushButton->setText(QCoreApplication::translate("Widget", "Submit", nullptr));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class Widget: public Ui_Widget {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_WIDGET_H
